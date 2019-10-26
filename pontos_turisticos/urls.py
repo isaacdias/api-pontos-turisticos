a9cfe825ede7d37c057275d7f8f254698ec746a3
Isaac Dias <isaacardias@gmail.com>
2019-10-26 12:53:02 -0300

    configurada a router de pontoturistico

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from core.api.viewsets import PontoTuristicoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', PontoTuristicoViewSet)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
