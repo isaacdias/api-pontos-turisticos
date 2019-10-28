0aa88ccf2913fff3d3e6b3ef9e3d9530ec5a2554
Isaac Dias <isaacardias@gmail.com>
2019-10-28 20:31:18 -0300

    criando o endpoint comentario

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -20,11 +20,13 @@ from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracaoViewSet)
 router.register(r'avaliacoes', AvaliacaoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
 
 
 urlpatterns = [
