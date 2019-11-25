4a36b14c786574ff8cfca22be51e1740a0ecf8d8
Isaac Dias <isaacardias@gmail.com>
2019-11-25 20:29:31 -0300

    Implementando a action denunciar

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PontoTuristicoSerializer
@@ -27,4 +28,8 @@ class PontoTuristicoViewSet(ModelViewSet):
         pass
 
     def partial_update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
         pass
