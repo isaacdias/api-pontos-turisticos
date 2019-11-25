cf2c13400ceba10c04b96ae9d3ded0ac833e74a0
Isaac Dias <isaacardias@gmail.com>
2019-11-25 20:58:47 -0300

    trazendo de volta as funções utilizanso a classe super

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -13,22 +13,22 @@ class PontoTuristicoViewSet(ModelViewSet):
         return PontoTuristico.objects.filter(aprovado=True)
 
     def list(self, request, *args, **kwargs):
-        return Response({'teste':123})
+        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
 
     def partial_update(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
 
     @action(methods=['get'], detail=True)
     def denunciar(self, request, pk=None):
