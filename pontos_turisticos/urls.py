4717a1fd556e62e6dddb0d0be5e195450acdfa51
Isaac Dias <isaacardias@gmail.com>
2019-11-25 21:08:32 -0300

    configurando para fornecer imagens em ambiente de desenvolvimento

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
@@ -34,4 +36,4 @@ router.register(r'enderecos', EnderecoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
