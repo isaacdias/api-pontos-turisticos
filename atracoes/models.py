ff88086d558aa0483e97191eb32a0422f83bfb7b
Isaac Dias <isaacardias@gmail.com>
2019-11-25 21:14:39 -0300

    implementando o campo foto do endpoint atrações

--- atracoes/models.py
+++ atracoes/models.py
@@ -5,6 +5,8 @@ class Atracao(models.Model):
     descricao = models.TextField()
     horario_func = models.TextField()
     idade_minima = models.IntegerField()
+    foto = models.ImageField(upload_to='atracoes', null=True, blank=True)
+
 
     def __str__(self):
         return self.nome
