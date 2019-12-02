de1697db13352e8610316b8d71bdb8aac54521c8
Isaac Dias <isaacardias@gmail.com>
2019-12-02 20:09:20 -0300

    filtrando por query string

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -9,8 +9,23 @@ class PontoTuristicoViewSet(ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     serializer_class = PontoTuristicoSerializer
+
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+
+        if nome:
+            queryset = queryset.filter(nome=nome)
+
+        if descricao:
+            queryset = queryset.filter(descricao=descricao)
+
+        return queryset
 
     def list(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
