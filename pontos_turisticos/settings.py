2735803d2347ea15f819cceb1208c23a08038c18
Isaac Dias <isaacardias@gmail.com>
2019-12-03 19:52:38 -0300

    adicionando o django-filters no INSTALLED_APPS

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'core',
     'atracoes',
     'comentarios',
@@ -128,3 +129,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'imagens'
 
 MEDIA_URL = '/media/'
+
+
